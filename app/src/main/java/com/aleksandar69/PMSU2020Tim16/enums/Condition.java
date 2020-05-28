b01b400344e65a8e43dd027746ace246fcb04e45
Dragan Petrovic <draganmop@gmail.com>
2020-05-28 14:04:13 +0200

    Dodate enumeracije Condition i Operation.

---
+++ app/src/main/java/com/aleksandar69/PMSU2020Tim16/enums/Condition.java
@@ -0,0 +1,8 @@
+package com.aleksandar69.PMSU2020Tim16.enums;
+
+public enum Condition {
+    TO,
+    FROM,
+    CC,
+    SUBJECT
+}
---
+++ app/src/main/java/com/aleksandar69/PMSU2020Tim16/enums/Operation.java
@@ -0,0 +1,7 @@
+package com.aleksandar69.PMSU2020Tim16.enums;
+
+public enum Operation {
+    MOVE,
+    COPY,
+    DELETE
+}
